dc27bf19eb4665c1f23dce3bc819bc73d4b468d7
Juergen Kleisz <juergen.kleisz@gmail.com>
2011-11-24 15:12:38 +0100

    <Tag5> Clemens: Kommenta hinzufuegen

--- SortedSetExample/src/main/java/at/jku/ce/Main.java
+++ SortedSetExample/src/main/java/at/jku/ce/Main.java
@@ -9,11 +9,11 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		Set<DomainObject> objectSet = new HashSet<DomainObject>();
-		objectSet.add(new DomainObject("Helga"));
-		objectSet.add(new DomainObject("Walter"));
-		objectSet.add(new DomainObject("Hans"));
-		objectSet.add(new DomainObject("Paula"));
-		objectSet.add(new DomainObject("Claudia"));
+		objectSet.add(new DomainObject("Helga","asdf"));
+		objectSet.add(new DomainObject("Walter","asdf"));
+		objectSet.add(new DomainObject("Hans","asdf"));
+		objectSet.add(new DomainObject("Paula","asdf"));
+		objectSet.add(new DomainObject("Claudia","asdf"));
 		for (DomainObject domainObject : objectSet) {
 			LOGGER.debug(domainObject.id);
 		}
