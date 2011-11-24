180957470177e878d89c72e4ad096450a1bf9528
Juergen Kleisz <juergen.kleisz@gmail.com>
2011-11-24 14:03:53 +0100

    <Tag2> Clemens: getter Methode hinzugefuegt

--- SortedSetExample/src/main/java/at/jku/ce/DomainObject.java
+++ SortedSetExample/src/main/java/at/jku/ce/DomainObject.java
@@ -4,17 +4,21 @@ public class DomainObject {
 
 	public String id;
 	private String name;
-	
+
 	public DomainObject(String id) {
 		super();
 		this.id = id;
 	}
-	
+
 	/**
 	 * Default constructor
 	 */
 	public DomainObject() {
 		super();
+	}
+
+	public String getId() {
+		return id;
 	}
 
 }
